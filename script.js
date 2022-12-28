36c0c0ff918cecb8cc7cb41a1cadac2cb4daa851
shanto096 <shanto096@gmail.com>
2022-12-29 01:31:14 +0600

    final

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const monthEl = document.querySelector(".month h1");
+const fullDateEl = document.querySelector(".month p");
+const dayEl = document.getElementById("days");
+
+
+
+const monthInx = new Date().getMonth();
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1;
+
+
+
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "may",
+    "june",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+ monthEl.innerHTML =  months[monthInx]; 
+fullDateEl.innerHTML = new Date().toDateString();
+fullDateEl.style.color = "green"
+
+console.log(months[monthInx]);
+
+let days = "";
+for (let i = firstDay; i > 0; i--) {
+    days += `<div class="empty"></div>`
+}
+
+for (let i = 1; i <= lastDay; i++) {
+    if (i === new Date().getDate()) {
+        days += `<div class="bg-red-500 py-2 text-white  sm:w-full w-10">${i}</div>`;
+    }else{
+        days += `<div>${i}</div>`;
+    }
+    
+}
+dayEl.innerHTML = days;
